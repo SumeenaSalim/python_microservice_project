5731c73a8a29e8f011e555ec7ba67eb28f520a27
Sumeenasalim <sumeenas@alokin.in>
2023-10-04 22:15:19 +0530

    feat: connect with db

--- movie_service/movie_app/api/models.py
+++ movie_service/movie_app/api/models.py
@@ -1,9 +1,19 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
 
-class Movies(BaseModel):
+class MovieIn(BaseModel):
     title: str
     description: str
     type: List[str]
     casts: List[str]
     year: int
+
+class MovieOut(MovieIn):
+    id: int
+
+class MovieUpdate(MovieIn):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    type: Optional[List[str]] = None
+    casts: Optional[List[str]] = None
+    year: Optional[int] = None
