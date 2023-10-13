0ad63899f0162043ad395794609fe0cb4fcebf35
Sumeenasalim <sumeenas@alokin.in>
2023-10-13 18:35:19 +0530

    feature: Added cast service

--- movie_service/app/api/view.py
+++ movie_service/app/api/view.py
@@ -2,42 +2,91 @@ from fastapi import APIRouter, HTTPException
 from typing import List
 
 from app.api.models import MovieOut, MovieIn
-from app.api.db_api import get_movies, get_movie, add_movie, update_movie, delete_movie
+from app.api.db_api import get_all_movies, fetch_movie, add_movie, edit_movie, delete_movie
+from app.api.service import is_cast_present
 
 movie = APIRouter()
 
 
 @movie.get('/', response_model=List[MovieOut])
-async def get_movie():
-    try:
-        resp = await get_movies()
-    except Exception:
+async def get_movies():
+    resp = await get_all_movies()
+    if not resp:
         raise HTTPException(status_code=404, detail="Connection failed")
-    return resp
+  
+    return {
+        "message": "success",
+        "data": resp
+    }
+
+
+@movie.get('/{id}', response_model=MovieOut)
+async def get_movie_by_id(id: int):
+    movie = await fetch_movie(id)
+    print("\n\n\n", movie)
+    if not movie:
+        raise HTTPException(status_code=404, detail=f"Movie with given id {id} not found")
+    return {
+        "message": "success",
+        "data": movie
+    }
 
 
 @movie.post('/', status_code=201)
-async def add_movie(payload: MovieIn):
+async def create_movie(payload: MovieIn):
+    for c_id in payload.casts_id:
+        if not is_cast_present(c_id):
+            raise HTTPException(status_code=404, detail=f"Cast with id:{c_id} does not exist")
+
     movie_id = await add_movie(payload)
-    return {'id': movie_id, **payload.model_dump()}
+    return {
+        "message": "success",
+        "data": {
+            "id": movie_id,
+            "title": payload.title,
+            "description": payload.description,
+            "type": payload.type,
+            "casts": payload.casts,
+            "year": payload.year,
+        }        
+    }
 
 
 @movie.put('/{id}')
 async def update_movie(id: int, payload: MovieIn):
-    movie = await get_movie(id)
-    if movie:
-        update_data = payload.model_dump(exclude_unset=True)
-        movie_in_db = MovieIn(**movie)
+    movie = await fetch_movie(id)
+    if not movie:
+        raise HTTPException(status_code=404, detail=f"Movie with given id {id} does not exist")
+    
+    if 'casts_id' in payload:
+        for c_id in payload.casts_id:
+            if not is_cast_present(c_id):
+                raise HTTPException(status_code=404, detail=f"Cast with given id:{c_id} does not exist")
 
-        updated_movie = movie_in_db.model_copy(update=update_data)
-        return await update_movie(id, updated_movie)
-    else:
-        raise HTTPException(status_code=404, detail="Movie with given id not found")
+    movie_id = await edit_movie(id, payload)
+    return {
+        "message": "success",
+        "data": {
+            "id": movie_id,
+            "title": payload.title,
+            "description": payload.description,
+            "type": payload.type,
+            "casts": payload.casts,
+            "year": payload.year,
+        }        
+    }    
     
 
 @movie.delete('/{id}')
-async def delete_movie(id: int):
-    found = await get_movie(id)
+async def remove_movie(id: int):
+    found = await fetch_movie(id)
     if not found:
-        raise HTTPException(status_code=404, detail="Movie with given id not found")
-    return await delete_movie(id)
+        raise HTTPException(status_code=404, detail=f"Movie with given id {id} does not exist")
+    await delete_movie(id)
+    return {
+        "message": "success",
+        "data": {
+            "id": id,
+            "deleted": True
+        }
+    }
