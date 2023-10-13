89149267a03c5df43354b52877dc18060917b30f
Sumeenasalim <sumeenas@alokin.in>
2023-10-13 14:23:34 +0530

    fix: Updated apis for more standardized

--- movie_service/app/api/db_api.py
+++ movie_service/app/api/db_api.py
@@ -2,27 +2,34 @@ from fastapi import HTTPException
 from app.api.models import MovieIn
 from app.api.db import database, movies
 
-
-async def get_movies():
+async def get_all_movies():
     query = movies.select()
     return await database.fetch_all(query=query)
 
-
 async def add_movie(data: MovieIn):
     if data:
-        query = movies.insert().values(**data)
-        return await database.execute(query=query)
+        title_check_query = movies.select(movies.c.title==data.title)
+        title_result = database.fetch_one(query=title_check_query)
+        if not title_result:
+            query = movies.insert().values(title=data.title, description=data.description, type=data.type, casts=data.casts, year=data.year)
+            return await database.execute(query=query)
+        else:
+            raise HTTPException(status_code=400, detail=f"Movie {data.title} already exists")
     else:
-        raise HTTPException(status_code=400, detail='Invalid data')
-    
-    
-async def get_movie(id: int):
+        raise HTTPException(status_code=400, detail="Invalid data")
+
+async def fetch_movie(id: int):
     query = movies.select(movies.c.id == id)
     return await database.fetch_one(query=query)
 
-    
-async def update_movie(id: int, data: MovieIn):
-    query = movies.update().where(movies.c.id == id).values(**data)
+async def edit_movie(id: int, data: MovieIn):
+    query = movies.update().where(movies.c.id == id).values(
+        title=data.title,
+        description=data.description,
+        type=data.type,
+        casts=data.casts,
+        year=data.year
+    ).returning(movies.c.id)
     return await database.execute(query=query)
 
 async def delete_movie(id: int):
--- movie_service/app/api/view.py
+++ movie_service/app/api/view.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from typing import List
 
 from app.api.models import MovieOut, MovieIn
-from app.api.db_api import get_movies, get_movie, add_movie, update_movie, delete_movie
+from app.api.db_api import get_all_movies, fetch_movie, add_movie, edit_movie, delete_movie
 
 movie = APIRouter()
 
@@ -10,34 +10,60 @@ movie = APIRouter()
 @movie.get('/', response_model=List[MovieOut])
 async def get_movie():
     try:
-        resp = await get_movies()
+        resp = await get_all_movies()
     except Exception:
         raise HTTPException(status_code=404, detail="Connection failed")
-    return resp
+    return {
+        "message": "success",
+        "data": resp
+    }
 
 
 @movie.post('/', status_code=201)
-async def add_movie(payload: MovieIn):
+async def create_movie(payload: MovieIn):
     movie_id = await add_movie(payload)
-    return {'id': movie_id, **payload.model_dump()}
+    return {
+        "message": "success",
+        "data": {
+            "id": movie_id,
+            "title": payload.title,
+            "description": payload.description,
+            "type": payload.type,
+            "casts": payload.casts,
+            "year": payload.year,
+        }        
+    }
 
 
 @movie.put('/{id}')
 async def update_movie(id: int, payload: MovieIn):
-    movie = await get_movie(id)
-    if movie:
-        update_data = payload.model_dump(exclude_unset=True)
-        movie_in_db = MovieIn(**movie)
-
-        updated_movie = movie_in_db.model_copy(update=update_data)
-        return await update_movie(id, updated_movie)
-    else:
-        raise HTTPException(status_code=404, detail="Movie with given id not found")
+    movie = await fetch_movie(id)
+    if not movie:
+        raise HTTPException(status_code=404, detail=f"Movie with given id {id} not found")
+    movie_id = await edit_movie(id, payload)
+    return {
+        "message": "success",
+        "data": {
+            "id": movie_id,
+            "title": payload.title,
+            "description": payload.description,
+            "type": payload.type,
+            "casts": payload.casts,
+            "year": payload.year,
+        }        
+    }    
     
 
 @movie.delete('/{id}')
-async def delete_movie(id: int):
-    found = await get_movie(id)
+async def remove_movie(id: int):
+    found = await fetch_movie(id)
     if not found:
-        raise HTTPException(status_code=404, detail="Movie with given id not found")
-    return await delete_movie(id)
+        raise HTTPException(status_code=404, detail=f"Movie with given id {id} not found")
+    await delete_movie(id)
+    return {
+        "message": "success",
+        "data": {
+            "id": id,
+            "deleted": True
+        }
+    }
